466950c82fd02e6d786338706231c713296a9855
Marcin Polak <mapoart@gmail.com>
2020-09-10 16:25:12 +0200

    Try of json parse,modify

--- templates/default.bat
+++ templates/default.bat
@@ -1,8 +1,24 @@
-import json
+@if (@CodeSection == @Batch) @then
+@echo off & setlocal
 
-let element = "Hydrogen"
-let atomicNumber = 1
+set "JSON={ "year": 2016, "time": "05:01" }"
 
-let jsonObject = %* {"element": element, "atomicNumber": atomicNumber}
-# This will print {"element":"Hydrogen", "atomicNumber": 1}
-echo $jsonObject
+rem // re-eval self with JScript interpreter and capture results
+for /f "delims=" %%I in ('cscript /nologo /e:JScript "%~f0"') do set "%%~I"
+
+rem // output captured results
+set JSON[
+
+rem // end main runtime
+goto :EOF
+
+@end // end Batch / begin JScript hybrid code
+
+var htmlfile = WSH.CreateObject('htmlfile'),
+    txt = WSH.CreateObject('Wscript.Shell').Environment('process').Item('JSON');
+
+htmlfile.write('<meta http-equiv="x-ua-compatible" content="IE=9" />');
+var obj = htmlfile.parentWindow.JSON.parse(txt);
+htmlfile.close();
+
+for (var i in obj) WSH.Echo('JSON[' + i + ']=' + obj[i]);
